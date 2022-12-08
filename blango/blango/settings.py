daf670f285a051e2e5550ce4dc889cb9046df34c
O-Semenov <world.shit@yandex.ru>
2022-12-08 18:21:00 +0300

    Finish crispy forms

--- blango/blango/settings.py
+++ blango/blango/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- blango/blog/views.py
+++ blango/blog/views.py
@@ -2,11 +2,38 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormMixin
+from django.shortcuts import redirect
+from blog.forms import CommentForm
 
 class index(ListView):
     model = Post
     template_name = 'blog/index.html'
 
-class post_detail(DetailView):
+class post_detail(FormMixin, DetailView):
     model = Post
     template_name = 'blog/post-detail.html'
+    form_class = CommentForm
+
+    def get_context_data(self, **kwargs):
+        context = super(post_detail, self).get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()  # Your comment form
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if request.user.is_active:
+            if request.method == "POST":
+                comment_form = CommentForm(request.POST)
+
+                if comment_form.is_valid():
+                    comment = comment_form.save(commit=False)
+                    comment.content_object = self.object
+                    comment.creator = request.user
+                    comment.save()
+                    return redirect(request.path_info)
+            else:
+                comment_form = CommentForm()
+        else:
+            comment_form = None
